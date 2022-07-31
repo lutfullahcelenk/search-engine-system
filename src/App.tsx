f012eeac32470ef070b82a14674977e03ebdb6f6
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-08-01 01:01:28 +0300

    assets and datas are taken from json

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // toastify
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+//context
+import { engineContext } from "./context/engineContext";
+//store
+import { initialStates } from "./store/initialStates";
+//pages
+import Home from "./pages/Home";
+import AddPage from "./pages/AddPage";
+import ResultPage from "./pages/ResultPage";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [data, setData] = useState(initialStates);
+  const [text, setText] = useState("");
 
-export default App
+  return (
+    <Router>
+      <engineContext.Provider
+        value={{
+          data,
+          text,
+          setText,
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resultpage" element={<ResultPage />} />
+          <Route path="/addpage" element={<AddPage />} />
+        </Routes>
+      </engineContext.Provider>
+    </Router>
+  );
+};
+
+export default App;
--- src/store/initialStates.ts
+++ src/store/initialStates.ts
@@ -1,4 +1,4 @@
-export const data = [
+export const initialStates = [
   [
     "Hyacinth Vincent",
     "Duis Corporation",
