899d8ac32f4f8ffa4c14b06f3ccf1761749a0139
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-08-01 00:46:19 +0300

    react router dom is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// toastify
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const App = () => {
   return (
