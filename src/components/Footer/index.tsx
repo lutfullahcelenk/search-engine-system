e01880ccd43dd0f36d0161c50be871409d2db2a7
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-08-02 04:16:14 +0300

    map is added to footer section

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,6 @@
 //@ts-nocheck
 import React from "react";
-//assets
 import laptop from "../../assets/laptop.png";
-//components
-import Map from "../Map";
 
 const Footer = () => {
   return (
@@ -24,7 +21,13 @@ const Footer = () => {
         </p>
       </div>
 
-      <Map />
+      <iframe
+        title="tesodev"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12041.118984277!2d28.8909481!3d41.0191353!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc98f44e9057adcde!2zVGVzb2RldiBZYXrEsWzEsW0gRG9uYW7EsW0gQmlsacWfaW0gQml5b21lZGlrYWwgS29uZ3JlIFR1cml6bSBFxJ9pdGltIERhbsSxxZ9tYW5sxLFrIExpbWl0ZWQgxZ5pcmtldGk!5e0!3m2!1str!2str!4v1659273691612!5m2!1str!2str"
+        className="w-full lg:w-1/3"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 };
